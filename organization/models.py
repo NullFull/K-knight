c9733fb7cf8d50047642b5e13f1489859bd8613b
김정환 <iz4blue@users.noreply.github.com>
2019-10-29 01:15:00 +0900

    리포터가 없으면 자동으로 등록함 리포터가 있으면 마지막을 찾아서 일단 보여줌

--- organization/models.py
+++ organization/models.py
@@ -11,12 +11,25 @@ class ThePress(models.Model):
 
 
 class Reporter(models.Model):
-    pass
+    def __str__(self):
+        candidate = self.reporterdetail_set
+        if candidate.exists():
+            return candidate.last()
 
 
 class ReporterDetail(models.Model):
-    reporter = models.ForeignKey('Reporter', on_delete=models.CASCADE)
+    reporter = models.ForeignKey('Reporter', default=None, blank=True, on_delete=models.CASCADE)
     press = models.ForeignKey('ThePress', on_delete=models.CASCADE)
     name = models.CharField('이름', max_length=32, default='', blank=True)
     email = models.EmailField('이메일', default='', blank=True)
     # 기자 공개 이미지 URL
+
+    def __str__(self):
+        return f"{self.press}: {self.name}[{self.email}]"
+
+    def save(self, *args, **kwargs):
+        # FIXME: 이렇게 하는게 맞으려나?
+        if self.reporter_id is None:
+            self.reporter = Reporter.objects.create()
+
+        super().save(*args, **kwargs)
