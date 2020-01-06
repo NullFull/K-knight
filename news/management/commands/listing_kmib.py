867d792b8b464103647d76aaed9200fb86585cb7
김정환 <iz4blue@users.noreply.github.com>
2020-01-07 00:50:29 +0900

    디버깅 코드 삭제 비슷한 패턴 합치는 중

--- news/management/commands/listing_kmib.py
+++ news/management/commands/listing_kmib.py
@@ -1,5 +1,5 @@
 import random
-from urllib.parse import urlparse
+import time
 from datetime import datetime, timedelta
 
 import pytz
@@ -8,16 +8,15 @@ from bs4 import BeautifulSoup
 from django.core.management import BaseCommand
 from django.utils import timezone
 
+from news.models import Article
+from organization.models import ThePress
 
 
 class Command(BaseCommand):
     help = '국민일보 최신기사 리스트를 DB에 추가한다'
-    press = None
     name = '국민일보'
+    press = ThePress.find_by_title(title=name)
     url_base = 'https://kmib.co.kr'
-
-    headers = {'User-Agent': 'Mozilla/5.0 ' + '(Windows NT 10.0; Win64; x64) ' +
-                             'AppleWebKit/537.36 (KHTML, like Gecko) ' + 'Chrome/60.0.3112.113 Safari/537.36'}
 
     # def add_arguments(self, parser):
     #     parser.add_argument('press_keyword', type=str)
@@ -34,22 +33,23 @@ class Command(BaseCommand):
         if now.hour < 6:
             dates.append(now - timedelta(1)) # requirement: 6시간 이상 지나지 않았으면 전날도 크롤링
 
-        print(dates)
         for adate in dates:
             for page in range(10, 0, -1):
                 url = f'http://news.kmib.co.kr/article/list.asp?sid1=all&sid2=&page={page}&sdate={adate:%Y%m%d}&st='
                 urls.append(url)
-                print(url)
 
         for url in urls:
             try:
-                _response = requests.get(url, headers=self.headers)
-                _response.encoding = 'cp949'
+                _header = {
+                    'User-Agent': self.press.user_agent
+                }
+                _response = requests.get(url, headers=_header)
+                _response.encoding = self.press.encoding
                 _response.close()
 
             except requests.exceptions.ConnectionError as e:
                 _second = random.randrange(5 * 60, 15 * 60)
-                # sleep_with_message(_second, '리스트가 읽히지 않습니다. 차단을 피하기 위해서 긴 대기 시간을 가집니다.')
+                time.sleep(_second)
                 exit()
 
             soup = BeautifulSoup(_response.text, 'lxml')
@@ -61,9 +61,16 @@ class Command(BaseCommand):
                 title = dt.string
                 datetime_str = item.select_one('dd.date').string
                 datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M')
+                datetime_obj = timezone.make_aware(datetime_obj, timezone=pytz.timezone('Asia/Seoul'), is_dst=False)
                 summary = item.select_one('dd.tx').string.strip()
                 thumbnail_obj = item.select_one('p.pic a img')
-                if thumbnail_obj: thumbnail_src = thumbnail_obj['src']
+                if thumbnail_obj:
+                    thumbnail_src = thumbnail_obj['src']
+
+                if Article.create_new(press=self.press, url=url, title=title, datetime=datetime_obj) is None:
+                    time.sleep(10)
+                    continue
 
                 print(f'국민일보: {datetime_obj}: {title}: {url}')
 
+            time.sleep(10)
